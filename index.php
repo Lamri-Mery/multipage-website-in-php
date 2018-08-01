78c5b5c214918b1b0ca910e723e45778e229e8e7
Lamri-Mery <Lamri.Mery@gmail.com>
2018-08-01 16:35:05 +0200

    seconde chance

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 		<title>Les petits vieux</title>
 
 		<!-- CSS -->
-		<link rel="stylesheet" href="assets/css/reset.css">
-		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" type="text/css" href="libraries/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -17,9 +18,53 @@
 		<![endif]-->
 	</head>
 	<body>
-			<img src="assets/img/logo.png">
+			<!-- <img src="assets/img/logo.png"> -->
+			<nav class="navbar navbar-fixed-top navbar-default" role="navigation">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- <a class="navbar-brand" href="#"><img src=""></a> -->
+					</div>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link</a>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</nav>
+			<div class="container-fluid">
+					<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+						<img src="assets/img/cat_dog.jpg">
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+						<div class="mypost">
+							<p>ATTENTION, notre refuge sera fermé exceptionnellement aux visiteurs ce samedi 21 juillet !</p>
+						</div>
+					</div>
+			</div>
+
+
+			<!-- <form class="" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form> -->
 		<!-- jQuery & JavaScript -->
-		<script src=""></script>
-		<script src=""></script>
+		<script src="libraries/jquery.min.js"></script>
+		<script src="libraries/bootstrap.min.js"></script>
 	</body>
 </html
