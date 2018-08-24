15c00989f50612add41754a7f16fbb71dbf59627
Lamri-Mery <Lamri.Mery@gmail.com>
2018-08-24 12:52:29 +0200

    clean index

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 
+<?php ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL); ?>
 <?php require 'partials/json.php'; ?>
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/menu.php'; ?>
@@ -9,19 +12,3 @@
 <?php require 'partials/pagination.php'; ?>
 <?php require 'partials/modal.php'; ?>
 <?php require 'partials/footer.php'; ?>
-
-<?php 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-	$jsonsubmit = isset($_POST['jsubmit']);
-	if ($jsonsubmit == true) {
-		// récupérer toute les données de mon formulaire et le mettre dans un tableau nommé mygoldbook
-		$mygoldbook = array();
-		
-		$mygoldbook['lastname'] = $_POST['jlastname'];
-		$mygoldbook['firstname'] = $_POST['jfirstname'];
-		$mygoldbook['message'] = $_POST['jmessage'];
-		$mygoldbook['date'] = date('d/m/Y à H:i');
-	}
-?>
